cbafa0eb7fe518327d5d5d445dcf445a6a6225de
Cody Boyko <codyboyko@yahoo.ca>
2015-11-13 15:36:51 -0500

    Update main controller to Encode/Decode string.

--- js/caesarCipher.js
+++ js/caesarCipher.js
@@ -1,19 +1,18 @@
 angular.module('caesarCipherApp', [])
   .controller('CaesarCipherController', ['$scope', function($scope) {
   	$scope.submit = function() {
-  		$scope.results = []; // Array of all string options
-
+  		$scope.results = []; // Array of all string options  		
   		// Loop over all letters of the alphabet/key options
   		for (var i = 0; i < 25; i++) {
+  			$scope.tempString = "";
   			// Loop over each char of string 
-  			for (var i = 0; i < Things.length; i++) {
+  			for (var x = 0; x < $scope.yourString.length; x++) {
   				// Build new string by adding i value
-
-  				// Push new string to array
-
+  				var letter = $scope.yourString.charAt(x);
+  				$scope.tempString += String.fromCharCode(letter.charCodeAt()+i);
   			};
-  			
-
+  			// Push new string to array
+  			$scope.results.push($scope.tempString);
   		};
   	};
   }]);

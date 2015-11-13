5ef62904f26d034a92a984bac6dc233f859f2d62
Cody Boyko <codyboyko@yahoo.ca>
2015-11-13 16:18:45 -0500

    Set ranges for characters.

--- js/caesarCipher.js
+++ js/caesarCipher.js
@@ -9,7 +9,25 @@ angular.module('caesarCipherApp', [])
   			for (var x = 0; x < $scope.yourString.length; x++) {
   				// Build new string by adding i value
   				var letter = $scope.yourString.charAt(x);
-  				$scope.tempString += String.fromCharCode(letter.charCodeAt()+i);
+  				console.log(letter);
+  				// Set encoding ranges ascii a=97, z=122 (-32 for upper case)
+  				// A=65, Z=90
+  				// TODO: Clean this up.
+  				if (letter.charCodeAt() >=65 && letter.charCodeAt() <=90) { // It's a capital
+  					if ( letter.charCodeAt()+i > 90) { // Over caps range so wrap
+  						$scope.tempString += String.fromCharCode(letter.charCodeAt()+i - 26);
+  					} else {
+  						$scope.tempString += String.fromCharCode(letter.charCodeAt()+i);
+  					}
+  				} else if (letter.charCodeAt() >=97 && letter.charCodeAt() <=122 ) { // It's lower case
+  					if ( letter.charCodeAt()+i > 122) { // Over lower case range so wrap
+  						$scope.tempString += String.fromCharCode(letter.charCodeAt()+i - 26);
+  					} else {
+  						$scope.tempString += String.fromCharCode(letter.charCodeAt()+i);
+  					}
+  				} else {				
+  					$scope.tempString += String.fromCharCode(letter.charCodeAt()+i);
+  				}
   			};
   			// Push new string to array
   			$scope.results.push($scope.tempString);
